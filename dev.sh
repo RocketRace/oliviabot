7034679bf161071bd291af13fc3af8e6767494e7
RocketRace <oliviaspalmu@gmail.com>
2024-06-17 12:59:29 +0300

    Add proper environment variables to dev.sh

--- dev.sh
+++ dev.sh
@@ -2,5 +2,5 @@
 trap 'kill 0' SIGINT
 # run in background
 cargo watch --quiet --exec check --shell 'touch .watch-trigger' &
-cargo watch --no-vcs-ignores --watch .watch-trigger --postpone --exec run &
+cargo watch --no-vcs-ignores --watch .watch-trigger --postpone --exec run --env DEV=1 &
 wait
