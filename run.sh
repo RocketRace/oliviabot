645ab694324cab985ebfb177a90795a3dcc721f5
RocketRace <oliviaspalmu@gmail.com>
2024-06-19 19:40:29 +0300

    fix run script ?

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-cargo watch --ignore-nothing --watch target/release/oliviabot -- ./target/release/oliviabot
+cargo watch --ignore-nothing --watch target/release/oliviabot --shell ./target/release/oliviabot
