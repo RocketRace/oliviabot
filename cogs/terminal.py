1b303c154a5a6dea1d3a29e98ca46e3d64676cba
RocketRace <oliviaspalmu@gmail.com>
2024-07-03 17:31:00 +0300

    refactor terminal

--- cogs/terminal.py
+++ cogs/terminal.py
@@ -24,107 +24,112 @@ def mention(inner: str) -> str:
 
 
 class TestContext(Context):
+    def format_node(self, node: parse_discord.Node) -> str:
+        match node:
+            case parse_discord.Text():
+                return node.text
+            case parse_discord.Bold():
+                return sgr(1) + self.format_markup(node.inner) + sgr(22)
+            case parse_discord.Italic():
+                return sgr(3) + self.format_markup(node.inner) + sgr(23)
+            case parse_discord.Underline():
+                return sgr(4) + self.format_markup(node.inner) + sgr(24)
+            case parse_discord.Strikethrough():
+                return sgr(9) + self.format_markup(node.inner) + sgr(29)
+            case parse_discord.Spoiler():
+                return colored(self.format_markup(node.inner), 37, 48, 5, 243)
+            case parse_discord.Quote():
+                return indent(self.format_markup(node.inner), colored("> ", 37))
+            case parse_discord.Header():
+                return indent(
+                    self.format_markup(node.inner),
+                    colored("#" * node.level + " ", 37),
+                )
+            case parse_discord.List():
+                out = []
+                if node.start:
+                    for i, item in enumerate(node.items):
+                        bullet = f"{node.start + i}. "
+                        out.append(
+                            indent(
+                                f"{bullet}{self.format_markup(item)}",
+                                " " * len(bullet),
+                            )
+                        )
+                else:
+                    for item in node.items:
+                        out.append(
+                            indent(
+                                f"- {self.format_markup( item)}",
+                                "  ",
+                            )
+                        )
+
+                return "\n".join(out)
+            case parse_discord.Link():
+                text = node.title if node.title else node.display_target
+                # hyperlink, limited terminal support
+                return f"\x1b[]8;;{node.target}\x1b\\{text}\x1b[]8;;\x1b\\"
+            case parse_discord.InlineCode():
+                return colored(node.content, 48, 5, 243)
+            case parse_discord.Codeblock():
+                # TODO
+                if node.language:
+                    return colored(f"```{node.language}\n{node.content}```", 48, 5, 243)
+                else:
+                    return colored(f"```\n{node.content}```", 48, 5, 243)
+            case parse_discord.UserMention():
+                if self.guild:
+                    member = self.guild.get_member(node.id)
+                    if member:
+                        return mention(f"@{member.nick}")
+                user = self.bot.get_user(node.id)
+                if user:
+                    return mention(f"@{user.name}")
+                return mention(f"<@{node.id}>")
+            case parse_discord.ChannelMention():
+                channel = self.bot.get_channel(node.id)
+                if channel and isinstance(
+                    channel, (discord.abc.GuildChannel, discord.Thread)
+                ):
+                    return mention(f"#{channel.name}")
+                return mention(f"<#{node.id}>")
+            case parse_discord.RoleMention():
+                if self.guild:
+                    role = self.guild.get_role(node.id)
+                    if role:
+                        name = role.name
+                        r, g, b = role.color.to_rgb()
+                        return colored(f"@{name}", 38, 2, r, g, b)
+                return mention(f"<@&{node.id}>")
+            case parse_discord.Everyone():
+                return mention("@everyone")
+            case parse_discord.Here():
+                return mention("@here")
+            case parse_discord.CustomEmoji():
+                return f":{node.name}:"
+            case parse_discord.UnicodeEmoji():
+                return node.char
+            case parse_discord.Timestamp():
+                try:
+                    datetime = node.as_datetime()
+                    return mention(datetime.strftime("%Y-%m-%d %H:%M:%S"))
+                except OverflowError:
+                    pass
+                if node.format:
+                    return mention(f"<t:{node.timestamp}:{node.format}>")
+                else:
+                    return mention(f"<t:{node.timestamp}>")
+
+            case other:
+                return parse_discord.formatting.format_markup(
+                    parse_discord.Markup([other])
+                )
+
     def format_markup(self, markup: parse_discord.Markup) -> str:
         formatted = ""
         for node in markup.nodes:
-            match node:
-                case parse_discord.Text():
-                    formatted += node.text
-                case parse_discord.Bold():
-                    formatted += sgr(1) + self.format_markup(node.inner) + sgr(22)
-                case parse_discord.Italic():
-                    formatted += sgr(3) + self.format_markup(node.inner) + sgr(23)
-                case parse_discord.Underline():
-                    formatted += sgr(4) + self.format_markup(node.inner) + sgr(24)
-                case parse_discord.Strikethrough():
-                    formatted += sgr(9) + self.format_markup(node.inner) + sgr(29)
-                case parse_discord.Spoiler():
-                    formatted += colored(self.format_markup(node.inner), 37, 48, 5, 243)
-                case parse_discord.Quote():
-                    formatted += indent(
-                        self.format_markup(node.inner), colored("> ", 37)
-                    )
-                case parse_discord.Header():
-                    formatted += indent(
-                        self.format_markup(node.inner),
-                        colored("#" * node.level + " ", 37),
-                    )
-                case parse_discord.List():
-                    out = []
-                    if node.start:
-                        for i, item in enumerate(node.items):
-                            bullet = f"{node.start + i}. "
-                            out.append(
-                                indent(
-                                    f"{bullet}{self.format_markup(item)}",
-                                    " " * len(bullet),
-                                )
-                            )
-                    else:
-                        for item in node.items:
-                            out.append(
-                                indent(
-                                    f"- {self.format_markup( item)}",
-                                    "  ",
-                                )
-                            )
-
-                    formatted += "\n".join(out)
-                case parse_discord.Link():
-                    text = node.title if node.title else node.display_target
-                    # hyperlink, limited terminal support
-                    formatted += f"\x1b[]8;;{node.target}\x1b\\{text}\x1b[]8;;\x1b\\"
-                case parse_discord.InlineCode():
-                    formatted += colored(node.content, 48, 5, 243)
-                case parse_discord.Codeblock():
-                    # TODO
-                    if node.language:
-                        formatted += colored(
-                            f"```{node.language}\n{node.content}```", 48, 5, 243
-                        )
-                    else:
-                        formatted += colored(f"```\n{node.content}```", 48, 5, 243)
-                case parse_discord.UserMention():
-                    if self.guild:
-                        member = self.guild.get_member(node.id)
-                        if member:
-                            formatted += mention(f"@{member.nick}")
-                    user = self.bot.get_user(node.id)
-                    if user:
-                        formatted += mention(f"@{user.name}")
-                    formatted += mention(f"<@{node.id}>")
-                case parse_discord.ChannelMention():
-                    channel = self.bot.get_channel(node.id)
-                    if channel and isinstance(
-                        channel, (discord.abc.GuildChannel, discord.Thread)
-                    ):
-                        formatted += mention(f"#{channel.name}")
-                    formatted += mention(f"<#{node.id}>")
-                case parse_discord.RoleMention():
-                    if self.guild:
-                        role = self.guild.get_role(node.id)
-                        if role:
-                            name = role.name
-                            r, g, b = role.color.to_rgb()
-                            formatted += colored(f"@{name}", 38, 2, r, g, b)
-                    formatted += mention(f"<@&{node.id}>")
-                case parse_discord.Everyone():
-                    formatted += mention("@everyone")
-                case parse_discord.Here():
-                    formatted += mention("@here")
-                case parse_discord.CustomEmoji():
-                    formatted += f":{node.name}:"
-                case parse_discord.UnicodeEmoji():
-                    formatted += node.char
-                case parse_discord.Timestamp():
-                    try:
-                        datetime = node.as_datetime()
-                        formatted += mention(datetime.strftime("%Y-%m-%d %H:%M:%S"))
-                    except OverflowError:
-                        if node.format:
-                            formatted += mention(f"<t:{node.timestamp}:{node.format}>")
-                        else:
-                            formatted += mention(f"<t:{node.timestamp}>")
+            formatted += self.format_node(node)
 
         return formatted
 
@@ -155,37 +160,52 @@ def indent(content: str, prefix: str) -> str:
 class Terminal(commands.Cog):
     """Terminal-based command execution for rapid local testing"""
 
-    def wait_for_response(self):
-        # Don't set timeouts, as asyncio.wait won't propagate them anyway
-        completion = asyncio.create_task(
-            self.bot.wait_for(
-                "command_completion",
+    async def wait_for_response(self):
+        try:
+            await self.bot.wait_for(
+                "command",
                 check=lambda ctx: ctx.author.id == self.bot.tester_bot_id,
-            ),
-            name="completion",
-        )
-        error = asyncio.create_task(
-            self.bot.wait_for(
-                "command_error",
-                check=lambda ctx, _: ctx.author.id == self.bot.tester_bot_id,
-            ),
-            name="error",
-        )
-        return asyncio.create_task(
-            asyncio.wait(
+                timeout=3.0,
+            )
+        except asyncio.TimeoutError:
+            return None
+        else:
+            print("Ack")
+            # Don't set timeouts, as asyncio.wait won't propagate them anyway
+            completion = asyncio.create_task(
+                self.bot.wait_for(
+                    "command_completion",
+                    check=lambda ctx: ctx.author.id == self.bot.tester_bot_id,
+                ),
+                name="completion",
+            )
+            error = asyncio.create_task(
+                self.bot.wait_for(
+                    "command_error",
+                    check=lambda ctx, _: ctx.author.id == self.bot.tester_bot_id,
+                ),
+                name="error",
+            )
+            return await asyncio.wait(
                 [completion, error],
-                timeout=10.0,
+                timeout=15.0,
                 return_when=asyncio.FIRST_COMPLETED,
             )
-        )
 
     def handle_response(
         self,
-        done: set[asyncio.Task[Any]],
-        pending: set[asyncio.Task[Any]],
+        response: tuple[set[asyncio.Task[Any]], set[asyncio.Task[Any]]] | None,
         *,
-        possibly_skipped: bool,
+        potentially_skipped: bool = False,
     ):
+        if response is None:
+            if potentially_skipped:
+                print("No response (possibly skipped)")
+            else:
+                print("Timed out")
+            return
+        else:
+            done, pending = response
         for task in pending:
             task.cancel()
         if done:
@@ -194,15 +214,11 @@ class Terminal(commands.Cog):
                     print("Command succeeded")
                 case "error":
                     print("Command errored")
-        elif possibly_skipped:
-            print("No reponse (possibly skipped)")
-        else:
-            print("No reponse")
 
     async def test_loop(self) -> None:
         if self.bot.terminal_cog_interrupted:
-            done, pending = await self.wait_for_response()
-            self.handle_response(done, pending, possibly_skipped=True)
+            response = await self.wait_for_response()
+            self.handle_response(response, potentially_skipped=True)
             self.bot.terminal_cog_interrupted = False
         while True:
             attempted = False
@@ -218,13 +234,12 @@ class Terminal(commands.Cog):
                 response = self.wait_for_response()
                 try:
                     attempted = True
-                    _, (done, pending) = await asyncio.gather(request, response)
+                    _, resp = await asyncio.gather(request, response)
                 except Exception:
                     logging.exception("Unhandled exception in command trigger")
-                    response.cancel()
                     continue
 
-                self.handle_response(done, pending, possibly_skipped=False)
+                self.handle_response(resp)
 
             except asyncio.CancelledError:
                 logging.info("Stopping terminal loop")
