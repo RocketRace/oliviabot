27c93855a8869e8e9bec254f3490b87a653f8ec2
RocketRace <oliviaspalmu@gmail.com>
2025-01-04 00:53:44 +0200

    qwd converter

--- cogs/gadgets/swish.py
+++ cogs/gadgets/swish.py
@@ -4,14 +4,32 @@ import re
 import discord
 from discord.ext import commands
 
-from bot import Context, Cog, qwd_only
+from bot import Context, Cog, OliviaBot, qwd_only
 
+class QwdieConverter(commands.Converter[discord.User]):
+    async def convert(self, ctx: commands.Context[OliviaBot], argument: str):
+        try:
+            return await commands.UserConverter().convert(ctx, argument)
+        except commands.MemberNotFound:
+            # try again
+            argument = argument.lower()
+        try:
+            return await commands.UserConverter().convert(ctx, argument)
+        except commands.MemberNotFound:
+            # todo add aliases (i'd copy from esobot but that's gpl3)
+            raise
 
 class Swish(Cog):
     @qwd_only()
     @commands.command()
-    async def swish(self, ctx: Context, user: discord.Member | discord.User, items: str, message: str | None = None):
-        """swish mig 100 000 kronor"""
+    async def swish(
+        self,
+        ctx: Context,
+        user: discord.User = commands.parameter(converter=QwdieConverter),
+        items: str = commands.parameter(), # blank param for syntax
+        message: str | None = None
+        ):
+        """swisha mig 100 000 kronor"""
         match = re.match(r"(\d*)(.*)", items)
         assert match
         amount, thing = match.groups()
@@ -25,4 +43,3 @@ class Swish(Cog):
             await user.send(f"🌀 Received **{amount} {thing}** from {ctx.author}!")
         else:
             await user.send(f"🌀 Received **{amount} {thing}** from {ctx.author} with message:\n>>> {message}")
-        
