2594436fc7265356c82a718a37c95cf18d78ec16
RocketRace <oliviaspalmu@gmail.com>
2025-01-15 20:26:26 +0200

    proxy split

--- cogs/meta/proxy.py
+++ cogs/meta/proxy.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 from bot import Context, Cog
 
 class Proxy(Cog):
-    @commands.command()
+    @commands.group(invoke_without_command=True)
     async def proxy(self, ctx: Context, value: bool | None = None):
         """Configure this bot to respond to proxy messages.
 
@@ -16,24 +16,30 @@ class Proxy(Cog):
         value: bool | None
             ("enable" / "disable") Whether to enable or disable proxy mode.
         """
-        proxied = await self.bot.is_proxied(ctx.author)
-        negation = "" if proxied else " not"
-        if value is None:
-            await ctx.send(
-                f"You have{negation} enabled proxy mode. "
-                "You can enable or disable it using `+proxy enable` or `+proxy disable`."
+        negation = "" if await self.bot.is_proxied(ctx.author) else " not"
+        await ctx.send(
+            f"You have{negation} enabled proxy mode. "
+            "You can enable or disable it using `+proxy enable` or `+proxy disable`."
+        )
+    
+    @proxy.command(name="enable", aliases=["on", "optin"])
+    async def proxy_enable(self, ctx: Context):
+        negation = "" if await self.bot.is_proxied(ctx.author) else " not"
+        async with ctx.cursor() as cur:
+            await cur.execute(
+                """INSERT OR IGNORE INTO proxiers VALUES(?);""", [ctx.author.id]
             )
-        else:
-            async with ctx.cursor() as cur:
-                if value:
-                    await cur.execute(
-                        """INSERT OR IGNORE INTO proxiers VALUES(?);""", [ctx.author.id]
-                    )
-                else:
-                    await cur.execute(
-                        """DELETE FROM proxiers WHERE user_id = ?;""", [ctx.author.id]
-                    )
-            action = "enabled" if value else "disabled"
-            await ctx.send(
-                f"You have {action} proxy mode (previously{negation} enabled)"
+        await ctx.send(
+            f"You have enabled proxy mode (previously{negation} enabled)"
+        )
+
+    @proxy.command(name="disable", aliases=["off", "optout"])
+    async def proxy_disable(self, ctx: Context):
+        negation = "" if await self.bot.is_proxied(ctx.author) else " not"
+        async with ctx.cursor() as cur:
+            await cur.execute(
+                """DELETE FROM proxiers WHERE user_id = ?;""", [ctx.author.id]
             )
+        await ctx.send(
+            f"You have disabled proxy mode (previously{negation} enabled)"
+        )
