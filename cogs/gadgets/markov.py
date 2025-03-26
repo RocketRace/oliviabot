f1ca5ae453840c56aa1747744e3ccdf0e2592a8b
RocketRace <git@olivialta.cc>
2025-03-26 18:22:29 +0200

    nompy

--- cogs/gadgets/markov.py
+++ cogs/gadgets/markov.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import discord
 from discord.ext import commands
-import numpy as np
 
 from bot import Context, Cog
 
@@ -24,6 +23,6 @@ class Chain(Cog):
         """
         tokens = [" ", "hello", "world"]
         token_ids = {" ": 0, "hello": 1, "world": 2}
-        counts = np.array([[0, 1, 1], [2, 0, 0], [2, 0, 0]])
-        weights = counts / counts.T.sum(0)
+        # counts = np.array([[0, 1, 1], [2, 0, 0], [2, 0, 0]])
+        # weights = counts / counts.T.sum(0)
         await ctx.send("acked")
