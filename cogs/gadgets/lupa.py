3f15905f7bb2727fcf7488abc8ed9b5da9e28fdb
RocketRace <git@olivialta.cc>
2025-05-25 00:40:40 +0300

    pupa template

---
+++ cogs/gadgets/lupa.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+import discord
+from discord.ext import commands
+
+from bot import Context, Cog
+
+class Pupa(Cog):
+    @commands.command(enabled=False, aliases=["pupa"])
+    async def lupa(self, ctx: Context):
+        '''the girls have a lot to say!'''
+        
+    
